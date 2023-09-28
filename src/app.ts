db830500231f5f0084d46f09074e46602cbba968
jahid <info.jahidulislamjahid@gmail.com>
2023-09-29 00:26:08 +0600

    core skeleton

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import routes from './app/routes';
 
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 
 app.use(cors());
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/v1', routes);
 
-app.get('/', async (req, res) => {
-  res.send(
-    'Welcome to Book Inventory '
-  );
-});
 
+app.use(globalErrorHandler);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
